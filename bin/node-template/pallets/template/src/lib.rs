e4daf0a38129919d24fd9448f4b3843bb3870c69
twinzies <kjaintara@gmail.com>
2022-08-18 19:28:09 +0530

    Add identity and pallet trait

--- bin/node-template/pallets/template/src/lib.rs
+++ bin/node-template/pallets/template/src/lib.rs
@@ -3,18 +3,75 @@
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://docs.substrate.io/reference/frame-pallets/>
+use frame_support::{
+	codec::{Decode, Encode, MaxEncodedLen},
+	traits::{
+		Currency,
+		ReservableCurrency,
+	},
+	weights::Weight,
+	scale_info::TypeInfo,
+	sp_runtime::{
+		RuntimeDebug,
+	},
+};
+
+use pallet_identity::IdentityField;
+type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
 pub use pallet::*;
+type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
+
+/// Interface required for identity verification.
+pub trait IdentityVerifier<AccountId> {
+	/// Function that returns whether an account has an identity registered with the identity provider.
+	fn has_identity(who: &AccountId, fields: u64) -> bool;
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub enum Vote{
+	Aye,
+	Nay,
+	Abstain,
+}
 
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-
+	use super::*;
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// The currency trait.
+		type Currency: ReservableCurrency<Self::AccountId>;
+
+		/// The origin which may forcibly set or remove a proposal.
+		type ForceOrigin: EnsureOrigin<Self::Origin>;
+
+		// The VoteIndex assigned to a proposal upon creation.
+		type VoteIndex: Get<u64>;
+
+		type VoteQty: Get<u64>;
+
+		// Interface decision for Yay / Nay votes left upto implementer.
+		type VoteYay: Eq;
+
+		type VoteNay: Eq;
+
+		/// The minimum time a proposal must be alive before being called.
+		#[pallet::constant]
+		type MinTime: Get<u32>;
+
+		/// Proposal fee.
+		#[pallet::constant]
+		type ProposalFee: Get<BalanceOf<Self>>;
+
+		/// Running count of a proposal
+		type Count: Get<u64>;
 	}
 
 	#[pallet::pallet]
